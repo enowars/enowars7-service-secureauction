ba46f17f318e3e913e5e780b7537d752fccd2c8a
KutalVolkan <vkutal@outlook.com>
2023-05-25 18:22:58 +0200

    working version without checker

--- service/bid.php
+++ service/bid.php
@@ -77,6 +77,14 @@
             $itemId = (int)$itemId;
             $userId = (int)$userId;
 
+             // Check if the new bid starts with 'eno' or is a valid number
+            $isEnoBid = substr($amount, 0, 3) == 'eno';
+            $isNumericBid = is_numeric($amount);
+            
+            if (!$isEnoBid && !$isNumericBid) {
+                die("Invalid bid. Please enter a valid number or start your bid with 'eno'.");
+            }
+
             // Add the bid to the database without escaping or prepared statements
             $insertQuery = "INSERT INTO bids (amount, user_id, item_id, created_at) VALUES ('$amount', $userId, $itemId, NOW())";
 
