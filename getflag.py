ebd1e488fea21f02bcfa2386637eec45a6e6ba90
KutalVolkan <vkutal@outlook.com>
2023-05-31 12:32:42 +0200

    getflag debugged worksgit add getflag.py !

--- getflag.py
+++ getflag.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 BASE_URL = "http://localhost:8080"  # Base URL of the web application
 USER_URL = f"{BASE_URL}/my_profile.php"  # URL for getting user bids
 
 # Function to sign up a new user
 def signup(user_name, password):
+    logging.debug('Attempting to sign up a new user.')
     signup_url = f"{BASE_URL}/signup.php"
     signup_data = {
         "user_name": user_name,
@@ -13,12 +17,13 @@ def signup(user_name, password):
     }
     response = requests.post(signup_url, data=signup_data)
     if response.status_code == 200:
-        print("User signed up successfully.")
+        logging.debug("User signed up successfully.")
     else:
-        print("Failed to sign up the user.")
+        logging.error("Failed to sign up the user.")
 
 # Function to log in and retrieve the session cookie
 def login(user_name, password):
+    logging.debug('Attempting to log in.')
     login_url = f"{BASE_URL}/login.php"
     login_data = {
         "user_name": user_name,
@@ -27,16 +32,17 @@ def login(user_name, password):
     session = requests.session()
     response = session.post(login_url, data=login_data)
     if response.status_code == 200:
-        print("User logged in successfully.")
+        logging.debug("User logged in successfully.")
         return session.cookies.get_dict()
     else:
-        print("Failed to log in.")
+        logging.error("Failed to log in.")
         return None
 
 # Function to get user bids
 def get_user_bids(cookie):
     page = 1
     while True:
+        logging.debug('Fetching bids for page %d.', page)
         response = requests.get(USER_URL, params={'page': page}, cookies=cookie)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "html.parser")
@@ -51,34 +57,41 @@ def get_user_bids(cookie):
                         start_price = cells[2].text.strip()
                         created_at = cells[3].text.strip()
                         bid_amount = cells[4].text.strip()
+                        logging.debug('Processing bid with Item ID: %s, Item Name: %s, Start Price: %s, Created At: %s, Bid Amount: %s', item_id, item_name, start_price, created_at, bid_amount)
                         if 'eno' in start_price.lower():  # Check if 'eno' is in the start price (case insensitive)
-                            print(f"Item ID: {item_id}, Item Name: {item_name}, Start Price: {start_price}, Created At: {created_at}, Bid Amount: {bid_amount}")
+                            logging.info('Found bid with "eno" in start price: Item ID: %s, Item Name: %s, Start Price: %s, Created At: %s, Bid Amount: %s', item_id, item_name, start_price, created_at, bid_amount)
                 else:
-                    print("No bids found.")
+                    logging.debug("No bids found on this page.")
                     break  # End the loop if there are no bids on this page
             else:
-                print("No bids found.")
+                logging.debug("No bids table found on this page.")
                 break  # End the loop if there is no bids table on this page
         else:
-            print("Failed to fetch user bids.")
+            logging.error("Failed to fetch user bids for page %d.", page)
             break  # End the loop if the request failed
         page += 1  # Increment the page number for the next iteration
-
-
 
 def main():
     # Sign up a new user
     user_name = "appuser_root"
     password = "testpassword"
-    signup(user_name, password)
+    logging.info('User name: %s', user_name)
+    # Uncomment the next line to enable user signup.
+    #signup(user_name, password)
 
     # Log in with the created user
+    logging.info('Logging in user %s.', user_name)
     cookie = login(user_name, password)
 
     if cookie:
+        logging.debug('Received session cookie: %s', cookie)
         # Get user bids
-        print("User Bids:")
+        logging.info('Fetching user bids.')
         get_user_bids(cookie)
+    else:
+        logging.error('Failed to get session cookie.')
 
 if __name__ == "__main__":
+    logging.info('Starting script.')
     main()
+    logging.info('Script finished.')
